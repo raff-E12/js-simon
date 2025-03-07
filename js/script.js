0fb6beaaf44f806492f53255a086707924fe422a
Raff_12 <raffelecoppola381@gmail.com>
2025-03-07 18:21:19 +0100

    feat: Modifica preliminare del progetto in questione

--- js/script.js
+++ js/script.js
@@ -19,19 +19,59 @@ const ContDownHandleInput = (counts, parentnode, inputnode)=>{
                 parentnode.classList.add('d-none');
                 inputnode.classList.remove('d-none');
             }
-    }, 1200);
+    }, 1100);
 }
 
 const NumberRandomGenHandle = (listparent)=>{
+    let Number_random = [];
     for (let index = 0; index < 5; index++) {
         let Number_number = Math.ceil(Math.random() * 50);
+        Number_random.push(Number_number);
         let list_number = document.createElement('li');
         list_number.setAttribute('data-number', Number_number);
         list_number.setAttribute('id', index);
         list_number.textContent = Number_number;
         listparent.append(list_number);
     }
+    return Number_random
 }
 
-NumberRandomGenHandle(cont_numbers);
+const HandleInputCheckEvent = (btn, numbersRandom, message)=>{
+
+    btn.addEventListener('click', (e) => {
+        let input_number_selection = document.querySelectorAll('.form-control');
+        let correct_numbers = [];
+        let incorrect_numbers = [];
+        let index = 0;
+
+        e.preventDefault();
+
+        while (index < input_number_selection.length) {
+            let data_number = Number(input_number_selection[index].value);
+            if (numbersRandom.includes(data_number)) {
+                correct_numbers.push(data_number);
+            } else {
+                incorrect_numbers.push(data_number);
+            }
+            index++;
+        }
+
+        if (correct_numbers.length === numbersRandom.length || numbersRandom.length === correct_numbers.length) {
+            message.classList.add('text-success');
+            message.classList.remove('text-danger');
+            message.textContent = `Complimenti! Hai indovinato tutti i numeri: ${numbersRandom.join('-')}`;
+        } else {
+            message.classList.add('text-danger');
+            message.classList.remove('text-success');
+            message.textContent = `Riprova! Hai indovinato: (${correct_numbers.join('-')}), ma i numeri errati sono: (${incorrect_numbers.join('-')}).`;
+        }
+    })
+
+}
+
+const RandomNumber = NumberRandomGenHandle(cont_numbers);
 ContDownHandleInput(index_contdown, cont_numbers, input_memory);
+
+const btn_Gen = document.getElementById('btn-conf');
+let text_message_sc = document.getElementById('message');
+HandleInputCheckEvent(btn_Gen, RandomNumber, text_message_sc)
